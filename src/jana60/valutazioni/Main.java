b6128939071098285b0c2a69f4086fede46ad582
EmanueleEsposito <manu2022@blu.it>
2022-06-16 16:19:33 +0200

    fine esercizio

--- src/jana60/valutazioni/Main.java
+++ src/jana60/valutazioni/Main.java
@@ -3,8 +3,31 @@ package jana60.valutazioni;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		// array di studenti
+		Studente rogerFederer = new Studente(1, 25, 4);
+		Studente rafaelNadal = new Studente(2, 50, 1);
+		Studente novakDjokovic = new Studente(3, 51, 3);
+		Studente andyMurray = new Studente(4, 35, 2);
+		Studente stanislasWawrinka = new Studente(5, 10, 5);
+		Studente[] arrayStudenti = { rogerFederer, rafaelNadal, novakDjokovic, andyMurray, stanislasWawrinka };
+
+		// stampare a video per ogni studente se è stato promosso o bocciato e quanti
+		// studenti sono stati promossi in tutto.
+		int studentiPromossi = 0;
+		for (int i = 0; i < arrayStudenti.length; i++) {
+			if (arrayStudenti[i].esitoFinale()) {
+				System.out.println(arrayStudenti[i].idStudente + "Promosso");
+				studentiPromossi++;
+
+			} else {
+				System.out.println(arrayStudenti[i].idStudente + "Bocciato");
+			}
+
+		}
+
+		// stampare il numero totale di studenti promossi
+		System.out.println("il numero totale di studenti promossi è: " + studentiPromossi);
 	}
 
 }
