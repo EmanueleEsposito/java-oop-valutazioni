c7d62ed29fce4af1323f7b5404d5d8f1414fb848
EmanueleEsposito <manu2022@blu.it>
2022-06-16 15:25:18 +0200

    aggiunti attributi,costruttore e metodo

--- src/jana60/valutazioni/Studente.java
+++ src/jana60/valutazioni/Studente.java
@@ -1,5 +1,31 @@
 package jana60.valutazioni;
 
 public class Studente {
+	// Attributi
+	int idStudente;
+	int percentualeAssenze;
+	double mediaVoti;
 
+	// Costruttore
+	public Studente(int idStudente, int percentualeAssenze, double mediaVoti) {
+		super();
+		this.idStudente = idStudente;
+		this.percentualeAssenze = percentualeAssenze;
+		this.mediaVoti = mediaVoti;
+	}
+
+	// metodo che calcola, in base a percentuale delle assenze e media dei voti, se
+	// lo studente è promosso o bocciato
+	boolean esitoFinale() {
+		boolean promosso = false;
+		if (percentualeAssenze > 50) {
+			promosso = false;
+		}
+		if (percentualeAssenze >= 25 && percentualeAssenze <= 50 && mediaVoti > 2) {
+			promosso = true;
+		} else if (percentualeAssenze < 25 && mediaVoti >= 2) {
+			promosso = true;
+		}
+		return promosso;
+	}
 }
